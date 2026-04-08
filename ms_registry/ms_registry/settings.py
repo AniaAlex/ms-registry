ec2024423504ed08f658392397dbdbeeeb1253c3
Anna Alexeeva <anna@sunet.se>
2026-04-08 12:12:17 +0200

    Add static files fix

--- ms_registry/ms_registry/settings.py
+++ ms_registry/ms_registry/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = "/var/www/static/"
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = "/var/www/media/"
 
 # Default primary key field type
