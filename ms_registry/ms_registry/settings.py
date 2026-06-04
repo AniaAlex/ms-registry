ef7d3d4f22e1c34e652c941418f87a50cae40b04
Anna Alexeeva <anna@sunet.se>
2026-06-04 11:09:17 +0200

    Add-the-cookie-authentication

--- ms_registry/ms_registry/settings.py
+++ ms_registry/ms_registry/settings.py
@@ -181,7 +181,7 @@ LOGGING = {
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "participant.authentication.JWTCookieAuthentication",
     ),
 }
 
